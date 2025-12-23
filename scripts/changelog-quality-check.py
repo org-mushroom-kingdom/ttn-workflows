d093cead43582d7751cb37e72f406abda903b484
Matt C <mcumm64@gmail.com>
2025-12-23 00:22:17 -0500

    comments>>c-q-c.py; env's,exp CHGLGname>>c-q-c.yml

---
+++ scripts/changelog-quality-check.py
@@ -0,0 +1,4 @@
+# Take in an argument of changed_files_str (a comma-delimited string)
+# Turn that str into an arr called changed_files
+# For each filename in changed_files
+#   Assess the filename is equal to the expected CHANGELOG filename
