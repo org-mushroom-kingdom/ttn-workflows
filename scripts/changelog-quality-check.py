682c0aac095c7d315cdb98ec920a109452feac9e
Matt C <mcumm64@gmail.com>
2025-12-29 13:42:28 -0500

    setup python and mod script
    Try to write to GITHUB_ENV with Python script

--- scripts/changelog-quality-check.py
+++ scripts/changelog-quality-check.py
@@ -20,12 +20,16 @@
     # sys.exit(1) ?? --> Pass up to workflow and have workflow output message and PR comment
     # The failure message = "No CHANGELOG files found in PR."
 
+import os
+
+
 changed_files = sys.argv[1]
 expected_changelog_name = sys.argv[2]
 
 file_beg_w_changelog_exists = False
 changed_files_arr = changed_files.split(',')
 files_beg_w_changelog = []
+github_env_file = os.getenv('GITHUB_ENV')
 
 for changed_filename in changed_files_arr:
     if changed_filename.startswith("CHANGELOG"):
@@ -40,9 +44,15 @@ if changelog_exists == true and changelog_naming_correct == true:
     output = "A CHANGELOG file was found and matches the expected naming convention for this repo and release branch."
 elif  file_beg_w_changelog_exists == true and changelog_naming_correct == false:
     output = "A CHANGELOG file was found (specifically, a file beginning with 'CHANGELOG' was found), but is not the correct name for this release branch. The name of the CHANGELOG file should be {expected_changelog_name}"
-else
+else:
     # Note: Make sure synchronize is specified in pull_request event in caller workflows.
     # TODO: Make sure \n and += work in Python as expected.
     output = "No CHANGELOG file found. All release branches going into main must have a CHANGELOG file in the pull request. \n"
     output+= "The name of the CHANGELOG file should be {expected_name}. \n"
     output+= "Please either amend this PR to include the CHANGELOG file, or close this PR and create a new one with the CHANGELOG file."
+
+# TODO: Test this and see if it works in the reusable workflow. If not, try doing the commented block below 
+# The "a" flair means open the file in append mode
+# with open(github_env_file, "a") as github_env:
+    # github_env.write("MY_VAR=MY_VALUE")
+github_env_file.write(f"{CHANGELOG_MSG}={output}\n")
