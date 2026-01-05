83194cdf3014125157737224ca075ca8bfdae1b4
Matt C <mcumm64@gmail.com>
2026-01-04 22:45:31 -0500

    rwrk p_c_f arr logic>>c-q-c.py; cmmnts>>c-q-c-.yml

--- scripts/changelog-quality-check.py
+++ scripts/changelog-quality-check.py
@@ -1,6 +1,8 @@
 # Take in an argument of changed_files_str (a comma-delimited string). Turn that str into an arr called changed_files_arr
-# Iterate thru arr to see if properly named CHANGELOG file exists. Output result msg and status code
-# Note: If more than one CHANGELOG file found, immediately fail regardless of passing CHANGELOG name.
+# Iterate thru arr: add to potential_changelog_files if filename begins with "CHANGELOG"
+# Note: If length of potential_changelog_files != 1, fail regardless of passing CHANGELOG name.
+# If length of potential_changelog_files == 1, pass
+# In whatever scenario, write an output message that the workflow can use in a PR comment
 
 import sys
 import os
@@ -10,9 +12,6 @@ def main():
     # Remember sys.argv[0] is the name of the script itself
     changed_files_str = sys.argv[1]
     expected_changelog_name = sys.argv[2]
-
-    file_beg_w_changelog_exists = False
-    changelog_naming_passes = False
 
     changed_files_arr = changed_files_str.split(',')
     potential_changelog_files = []
@@ -25,11 +24,6 @@ def main():
 
     for changed_filename in changed_files_arr:
         if changed_filename.startswith("CHANGELOG"):
-            file_beg_w_changelog_exists = True
-            if changed_filename == expected_changelog_name:
-                changelog_naming_passes = True
-                # TODO: Reassess if you really want to exit early, because even if exp name exists, multiple other CHANGELOG files might.
-                break
             print("changed filename starts with CHANGELOG")
             potential_changelog_files.append(changed_filename)
 
@@ -37,24 +31,26 @@ def main():
     if len(potential_changelog_files) >1:
         output = f"There are multiple files in the pull request that begin with CHANGELOG. {only_1_changelog_msg} {exp_name_msg}"
         # sys.exit(1)
+    elif len(potential_changelog_files) == 0:
+        # Note: Make sure synchronize is specified in pull_request event in caller workflows.
+        # TODO: Make sure <br> and += work in Python as expected.
+        output = f"No CHANGELOG file found. {only_1_changelog_msg}<br>"
+        output+= f"{exp_name_msg}<br>"
+        output+= f"Please either amend this PR to include the CHANGELOG file, or close this PR and create a new one with the CHANGELOG file."
+        print(f"{output}")
+        # sys.exit(1)
     else:
-        # TODO: Write output as Github Actions env var which is possible but I forget how 
-        if file_beg_w_changelog_exists and changelog_naming_passes:
+        # There must be only 1 potential CHANGELOG file to assess
+        changelog_file_of_interest = potential_changelog_files[0]
+        if potential_changelog_files[0] ==  expected_changelog_name:
             output = f"{changelog_found_msg} and matches the expected naming convention for this repo and release branch."
             print(f"{output}")
             # sys.exit(0)
-        elif file_beg_w_changelog_exists and not changelog_naming_passes:
+        elif:
             output = f"{changelog_found_msg}, but is not the correct name for this release branch. {exp_name_msg}"
             print(f"{output}")
             # sys.exit(1)
-        else:
-            # Note: Make sure synchronize is specified in pull_request event in caller workflows.
-            # TODO: Make sure \n and += work in Python as expected.
-            output = f"No CHANGELOG file found. {only_1_changelog_msg}<br>"
-            output+= f"{exp_name_msg}<br>"
-            output+= f"Please either amend this PR to include the CHANGELOG file, or close this PR and create a new one with the CHANGELOG file."
-            print(f"{output}")
-            # sys.exit(1)
+
 
     # TODO: Test this and see if it works in the reusable workflow. 
     # TODO: Rework this into a function
