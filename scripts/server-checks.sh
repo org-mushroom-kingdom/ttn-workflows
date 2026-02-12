146f71ec41501724acad941eb29e758a07deb7fb
Matt C <mcumm64@gmail.com>
2026-02-12 00:36:21 -0500

    Update server-checks.sh

--- scripts/server-checks.sh
+++ scripts/server-checks.sh
@@ -10,7 +10,7 @@
 SERVER_LIST="./docs/server-list.csv"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' 
+ENDCOLOR='\033[0m' 
 
 while IFS="," read -r server description
 do
@@ -18,7 +18,7 @@ do
     http_status_code=$(curl -s -o /dev/null -w "%{http_code}" https://example.com)
     if [[ $http_status_code = "200" ]]
     then
-        echo "HOORAY"
+        echo "${GREEN}HOORAY${ENDCOLOR}"
     fi
     echo "http_status_code = $http_status_code"
 done < "$SERVER_LIST"
