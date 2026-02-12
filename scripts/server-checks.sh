e3e5bca5f291d984e9921cc15d8d01f9c27ce885
Matt C <mcumm64@gmail.com>
2026-02-12 00:05:02 -0500

    Update server-checks.sh

--- scripts/server-checks.sh
+++ scripts/server-checks.sh
@@ -1,4 +1,4 @@
 # Note: You may get an HTTP 429 (too many requests) if you test this too much within a certain time period
-
+echo "pwd = $PWD"
 http_status_code=$(curl -s -o /dev/null -w "%{http_code}" https://example.com)
 echo "http_status_code = $http_status_code"
