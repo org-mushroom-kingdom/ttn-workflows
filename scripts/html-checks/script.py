5d3a735871a39ece9a12604fd29e479ce71e480d
Matt C <mcumm64@gmail.com>
2026-03-03 23:49:10 -0500

    python steps>>c-e.yml; html-chck: script,rquirmnts

---
+++ scripts/html-checks/script.py
@@ -0,0 +1,13 @@
+# import sys
+# import os
+
+from bs4 import BeautifulSoup
+
+def main():
+    print("main() running!")
+
+def getHTMLFiles():
+    print("Searching for HTML files within the changed files")
+
+if __name__ == "__main__":
+    main()
