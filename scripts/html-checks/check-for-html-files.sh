270df34cb2f9e0ff06cdff3570785cff2373490d
Matt C <mcumm64@gmail.com>
2026-03-08 23:32:00 -0400

    Update check-for-html-files.sh

--- scripts/html-checks/check-for-html-files.sh
+++ scripts/html-checks/check-for-html-files.sh
@@ -13,8 +13,9 @@ if [ "$event_name" = "workflow_dispatch" ]
 then 
     echo "HEY"
     # PR #9 in ttn-workflows has 2 .html files in it
-    echo "pr_number=9" >> $GITHUB_ENV
+    pr_number="9"
 fi
+
 # Use process substitution to have jq output be treated as stdin for mapfile. (use Github CLI command below to get file info (obj array)), then jq to get just the "path" from each obj. Use -r to remove double quotes from values otherwise they will be present in the string  )
 mapfile -t changed_filenames < <(gh pr view $pr_number --json files | jq -r '.files[].path')
 [ "${verbose}" == "true" ] && echo "About to start HTML file search..." || :
