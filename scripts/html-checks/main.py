0a0b97cf23c0ccfa76ff56b39f21891c55adb38c
Matt C <mcumm64@gmail.com>
2026-03-12 00:06:10 -0400

    Update main.py

--- scripts/html-checks/main.py
+++ scripts/html-checks/main.py
@@ -8,6 +8,7 @@ def main():
 
 def getHTMLFiles():
     print("Searching for HTML files within the changed files")
+    # Array of filenames "ex-file1.html,ex-file2.html"
 
 if __name__ == "__main__":
     main()
