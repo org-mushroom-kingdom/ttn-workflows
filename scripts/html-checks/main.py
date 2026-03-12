b70cc2d77dd028bb27806092cee37aa681eef3b5
Matt C <mcumm64@gmail.com>
2026-03-12 00:44:27 -0400

    Call Python Script; mian.py arr scaff

--- scripts/html-checks/main.py
+++ scripts/html-checks/main.py
@@ -1,4 +1,4 @@
-# import sys
+import sys
 # import os
 
 from bs4 import BeautifulSoup
@@ -9,6 +9,9 @@ def main():
 def getHTMLFiles():
     print("Searching for HTML files within the changed files")
     # Array of filenames "ex-file1.html,ex-file2.html"
-
+    html_files_str = sys.argv[1]
+    html_files_arr = html_files_str.split(',')
+    for html_filename in html_files_arr:
+        print(f"HTML filename = {html_filename}")
 if __name__ == "__main__":
     main()
