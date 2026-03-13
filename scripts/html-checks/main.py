d7fe4aa1d96110c24b4f70e97e2abb780c9de329
Matt C <mcumm64@gmail.com>
2026-03-12 23:21:45 -0400

    Update main.py

--- scripts/html-checks/main.py
+++ scripts/html-checks/main.py
@@ -1,7 +1,8 @@
 import sys
 # import os
 
-from bs4 import BeautifulSoup
+# Need BeautifulSoup to make the bs4 obj, Comment to TODO WHAT DOES IT DO
+from bs4 import BeautifulSoup, Comment
 
 def main():
     print("main() running!")
@@ -13,7 +14,16 @@ def findTODOs():
     html_files_arr = html_files_str.split(',')
     for html_filename in html_files_arr:
         print(f"HTML filename = {html_filename}")
+        # Open file --> Make bs4 obj? from each file's contents --> scan contents for TODOs, throw in arr?
         
+        # The with keyword ensures the file is closed once done with open() activity
+        # open() opens a file: "r" specifies read mode, encoding is the charset
+        with open(html_filename, "r", encoding="utf-8") as html_file:
+            html_contents = html_file.read()
+
+        # This creates a bs4 object, which can be accessed as a data structure
+        soup = BeautifulSoup(html_contents, 'html.parser')
+
 
 if __name__ == "__main__":
     main()
