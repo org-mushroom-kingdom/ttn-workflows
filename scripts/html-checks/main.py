b715af6379165fec7b9aa6133b80fe1405e7725f
Matt C <mcumm64@gmail.com>
2026-03-13 00:14:21 -0400

    Update main.py
    Here goes nothing. It's kind of exciting!

--- scripts/html-checks/main.py
+++ scripts/html-checks/main.py
@@ -23,7 +23,22 @@ def findTODOs():
 
         # This creates a bs4 object, which can be accessed as a data structure
         soup = BeautifulSoup(html_contents, 'html.parser')
+        # find_all finds all elements in an HTML doc, returning a ResultSet (ex. to find all links --> all_links = soup.find_all('a'))
+        # You can use the "string" argument to look for strings instead of tags. This argument takes a string, but can also take regex and functions. Only get the text INSIDE elements is obtained, not along with the tag (ex. if html contains <p>HELLO</p> only gets "HELLO" as output, not "<p>HELLO</p>")
+        # Comments aren't elements in a traditional sense and we don't want to pick up other elements, so need to use a lambda function
+        # A lambda function is a small, anonymous (nameless), inline function usually made once and immediately used. (format --> argument: expression)
+        # find_all feeds each line of text to the lambda. The lambda takes in "line_text" as an input, whose value is a line of text and class is NavigableString.
+        # isinstance(item, class) sees if "item" is an instance of "class" and returns true or false
+        # So basically this is saying "Read through all lines (NavigableStrings). Filter out lines that are identified as comments (instance of the Comment class)"
+        # find_all returns a ResultSet, which is a bs4 version of a Python list. 
+        # Each item in that ResultSet is a NavigableString, which is like a string++: in addition to the text contents, it also has navagational functionality.
+        # A Comment is a specialized subclass of NavigableString (All Comments are NavigableStrings, but not all NavigableStrings are comments)
+        comments = soup.find_all(string=lambda line_text: isinstance(line_text, Comment))
 
+        # At this point we have all the comments in the HTML
+        print(f"Comments in {html_filename}:")
+        for comment in comments:
+            print(f"{comment}")
 
 if __name__ == "__main__":
     main()
