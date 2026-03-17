dbf0b7b2e449cc2cb0f92c72b2f926e8b95922ba
Matt C <mcumm64@gmail.com>
2026-03-17 00:40:54 -0400

    Refactor this

--- scripts/html-checks/main.py
+++ scripts/html-checks/main.py
@@ -10,7 +10,7 @@ def main():
     print("main() running!")
     findTODOs()
 
-def findTODOs():
+def findTODOs() -> list[NavigableString]:
     
     # The <var>: <datatype> = <value> seen below here is know as type annotation. This is an optional Python way of defining/declaring variables. 
     html_files_str: str = sys.argv[1] # comma-separated string of filenames "path/.../ex-file1.html,path/.../ex-file2.html"
@@ -21,6 +21,8 @@ def findTODOs():
     cwd: str = os.getcwd()
     print(f"Current working directory: {cwd}")
     
+    files_with_bad_todos: list[str] = []
+
     for html_filename in html_files_arr:
         print("--------------")
         print(f"HTML filename = {html_filename}")
@@ -43,7 +45,7 @@ def findTODOs():
         # find_all returns a ResultSet, which is a bs4 version of a Python list. 
         # Each item in that ResultSet is a NavigableString, which is like a string++: in addition to the text contents, it also has navagational functionality.
         # A Comment is a specialized subclass of NavigableString (All Comments are NavigableStrings, but not all NavigableStrings are comments)
-        comments: list[str] = soup.find_all(string=lambda line_text: isinstance(line_text, Comment))
+        comments: list[Tag] = soup.find_all(string=lambda line_text: isinstance(line_text, Comment))
 
 
         # This is a list comprehension. It's a shorthand way to create a new list based on the values of an existing list
@@ -55,9 +57,12 @@ def findTODOs():
         improper_todos: list[str] = []
         for todo_comment in todo_comments:
             # print(f"Item in todo_comments: {todo_comment}")
-            # if the todo comment doesn't have a substring like "#>=4letters-(1-5 numbers)"
+            # re.Pattern is the result of re.compile(). It's an object you can call regex-based methods on, like search() (see below)
+            # This pattern is  "A #, followed by 4-6 letters, followed by a -, followed by (1-5 numbers)"
             jira_story_pattern: re.Pattern = re.compile(r"#[a-z]{4,6}-\d{1,5}", re.IGNORECASE)
-            match = jira_story_pattern.search(todo_comment)
+            
+            # search() returns a Match object if a match was found. It returns None if it wasn't (None evaluates to falsy)
+            match: bool = jira_story_pattern.search(todo_comment)
             if match: 
                 print(f"This TODO comment has a story number! {todo_comment}")
             else:
@@ -68,6 +73,8 @@ def findTODOs():
             print("The following comments have 'TODO' in them, but are missing corresponding Jira story numbers:")
             for bad_todo in todos_missing_jiras:
                 print(f"{bad_todo}")
+                files_with_bad_todos.append(html_filename)
+
 
 if __name__ == "__main__":
     main()
