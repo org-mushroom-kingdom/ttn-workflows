2c86075ed37c981b7a79c18513acc12a508088a6
Matt C <mcumm64@gmail.com>
2026-03-17 19:57:46 -0400

    Update main.py

--- scripts/html-checks/main.py
+++ scripts/html-checks/main.py
@@ -31,10 +31,10 @@ def main():
 
     if (len(files_with_bad_todos)):
         print("WARNING! SEE ABOVE MESSAGES!")
-        # exit 1
+        sys.exit(1)
     else:
         print("HAPPY PATH")
-        # exit 0
+        sys.exit(0)
         
     # print(f"todos[0] = {todos[0]}")
     # bad_todos = check_TODOs_for_jira_nums()
