1887979d9c7faa00837a140870e6fe7944658b86
Matt C <mcumm64@gmail.com>
2026-03-17 00:16:15 -0400

    Type annotation and regex exp

--- scripts/html-checks/main.py
+++ scripts/html-checks/main.py
@@ -16,20 +16,20 @@ def findTODOs():
     html_files_str: str = sys.argv[1] # comma-separated string of filenames "path/.../ex-file1.html,path/.../ex-file2.html"
     path: str = sys.argv[2]
     html_files_arr: list[str] = html_files_str.split(',')
+
     # html_files_arr = ["./docs/testing/html-checks/html/article-template.html"]
-    # soup: BeautifulSoup
-    cwd = os.getcwd()
+    cwd: str = os.getcwd()
     print(f"Current working directory: {cwd}")
     
     for html_filename in html_files_arr:
         print("--------------")
         print(f"HTML filename = {html_filename}")
         # Open file --> Make bs4 obj? from each file's contents --> scan contents for TODOs, throw in arr?
-        path_html_filename = path+"/"+html_filename
+        html_filepath: str = path+"/"+html_filename
         # The with keyword ensures the file is closed once done with open() activity
         # open() opens a file: "r" specifies read mode, encoding is the charset
-        with open(path_html_filename, "r", encoding="utf-8") as html_file:
-            html_contents = html_file.read()
+        with open(html_filepath, "r", encoding="utf-8") as html_file:
+            html_contents: str = html_file.read()
 
         # This creates a bs4 object, which can be accessed as a data structure
         soup = BeautifulSoup(html_contents, 'html.parser')
@@ -43,29 +43,19 @@ def findTODOs():
         # find_all returns a ResultSet, which is a bs4 version of a Python list. 
         # Each item in that ResultSet is a NavigableString, which is like a string++: in addition to the text contents, it also has navagational functionality.
         # A Comment is a specialized subclass of NavigableString (All Comments are NavigableStrings, but not all NavigableStrings are comments)
-        comments = soup.find_all(string=lambda line_text: isinstance(line_text, Comment))
+        comments: list[str] = soup.find_all(string=lambda line_text: isinstance(line_text, Comment))
 
-        # At this point we have all the comments in the HTML
-        # print(f"Comments in {html_filename}:")
-        # for comment in comments:
-        #     has_todo=""
-        #     print(f"{comment}")
-        #     # Filter out comments that contain the text "TODO"
-        #     if "TODO" in comment:
-        #         print(f"TODO!!!! {comment}")
-        #     else:
-        #         print(f"{comment}")
 
         # This is a list comprehension. It's a shorthand way to create a new list based on the values of an existing list
         # The structure of a list comprehenison is [<expression><iterable><filter condition (optional)>]
         # This is saying "For each comment (in the comments array), if the comment has "TODO" in it, keep it AS IS (thus the very first "comment") in this new list. 
         # You can map and filter in the same step. For example if the first "comment" after [ was replace with "comment.upper" you'd filter for all comments containing "TODO", then upper() to those
-        todo_comments = [comment for comment in comments if "TODO" in comment]
+        todo_comments: list[str] = [comment for comment in comments if "TODO" in comment]
         
         for todo_comment in todo_comments:
             print(f"Item in todo_comments: {todo_comment}")
-            # if the todo comment doesn't have a substring like "#>=4letters-(0-5 numbers)"
-            # jira_story_pattern = r"^#"
+            # if the todo comment doesn't have a substring like "#>=4letters-(1-5 numbers)"
+            jira_story_pattern: re.Pattern = re.compile(r"#[a-z]{4,6}-\d{1,5}", re.IGNORECASE)
             # match = re.search(jira_story_pattern,comment)
             # if 
 
